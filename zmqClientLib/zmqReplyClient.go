f0166afe8ad9f141b1e9a07b0fb0f57aba6fa7e0
vagrant <vagrant@localhost.localdomain>
2015-11-03 18:48:10 +0000

    all my packages for now

---
+++ zmqClientLib/zmqReplyClient.go
@@ -0,0 +1,24 @@
+package zmqClientLib
+
+import (
+	zmq "github.com/pebbe/zmq4"
+)
+
+func sub_thread(zmq_url_port string, topics []string, c chan string){
+	
+	subscriber, _ := zmq.NewSocket(zmq.SUB)
+	defer subscriber.Close()
+	subscriber.Connect(zmq_url_port)
+	
+	for _, topic := range topics {
+		subscriber.SetSubscribe(topic)
+	}
+	for {
+		//  Read envelope with address
+		address, _ := subscriber.Recv(0)
+		//  Read message contents
+		contents, _ := subscriber.Recv(0)
+		Trace.Println("[%s] %s\n", address, contents)
+		c <- contents
+	}	
+}
---
+++ zmqPubSubProxy/zmqPubSubProxy.go
@@ -0,0 +1,57 @@
+//
+//  Weather proxy device.
+//
+//  NOT TESTED
+//
+
+package main
+
+import (
+	zmq "github.com/pebbe/zmq4"
+	"log"
+	"time"
+)
+
+const frontend_url_port ="tcp://*:5559"
+const backend_url_port = "tcp://*:5560"
+
+func listener_thread() { 
+	pipe, _ := zmq.NewSocket(zmq.PAIR) 
+ 	pipe.Bind("inproc://pipe") 
+ 
+ 	//  Print everything that arrives on pipe 
+	for { 
+		msg, err := pipe.RecvMessage(0) 
+		if err != nil { 
+			break 		//  Interrupted 
+ 		} 
+ 		log.Println(msg) 		
+ 	} 
+} 
+
+func main() {
+
+	go listener_thread()
+	time.Sleep(time.Millisecond * 5)
+	
+	//  This is where the weather server sits
+	frontend, _ := zmq.NewSocket(zmq.XSUB)
+	defer frontend.Close()
+	frontend.Bind(frontend_url_port)
+	
+	//  This is our public endpoint for subscribers
+	backend, _ := zmq.NewSocket(zmq.XPUB)
+	defer backend.Close()
+	backend.Bind(backend_url_port)
+	
+	listener, _ := zmq.NewSocket(zmq.PAIR) 
+	listener.Connect("inproc://pipe") 
+
+	log.Println("0MQ proxy started!")
+	log.Println("Frontend protocl/url/port:", frontend_url_port)
+	log.Println("Backend protocol/url/port:", backend_url_port)
+	
+	//  Run the proxy until the user interrupts us
+	err := zmq.Proxy(frontend, backend, listener)
+	log.Fatalln("Proxy interrupted:", err)
+}
